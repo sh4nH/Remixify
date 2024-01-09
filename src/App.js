414fecda0558992d2f44d0a3f25dfe4e49ba93d5
sh4nH <e0866259@u.nus.edu>
2024-01-09 23:21:05 +0800

    add react files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Home from './components/Home';
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      {/* Add other components or content as needed */}
+    </div>
+  );
+};
+
+export default App;
